5aec6c73c602786b4efef8d19f9722b7bc356508
Ansel Colvin <anselcolvin@gmail.com>
2018-09-10 22:35:45 -0400

    Update

--- template/specs.php
+++ template/specs.php
@@ -78,8 +78,9 @@ get_header(); ?>
                   </ul>
                </p>
             <h4>Auxiliary Craft</h4>
-            <p>Shuttlebays: 1 | Shuttle Capacity: 4<br>
-            Shuttles:
+            <p>Shuttlebays: 1<br>
+            Shuttle Capacity: 4<br>
+            Attached Shuttles:
                   <ul>
                         <li>1x <a href="https://wiki.bravofleet.com/index.php?title=Type_9_shuttlecraft" target="_blank">Type-9 Shuttlecraft</a></li>
                         <li>1x <a href="https://wiki.bravofleet.com/index.php?title=Type_11_shuttlecraft" target="_blank">Type-11 Shuttlecraft</a></li>
