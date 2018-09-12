3bde487e1f1714bcf66bfce5d9c359770f2d940b
Ansel Colvin <anselcolvin@gmail.com>
2018-09-11 21:51:59 -0400

    Template Update

--- template/manifest.php
+++ template/manifest.php
@@ -152,7 +152,7 @@ These could be SPANs or DIVs and with entirely different classes.
                   Chief Medical Officer</p></div><div class="mani-field character"><p><a href="/enlist">Position Available</a></p></div>
          </div>
          <div class="row">
-            <div class="mani-field rank"><img src="/wp-content/themes/diligent/images/pips/navy_gold/t-o2.png" /></div><div class="mani-field position"><p class="tooltips" title="
+            <div class="mani-field rank"><img src="/wp-content/themes/diligent/images/pips/navy_gold/t-o3.png" /></div><div class="mani-field position"><p class="tooltips" title="
             Because of their training in psychology, technically the ship's/facility's Counselor is considered part of Starfleet Medical. The Counselor is responsible 
             both for advising the CO in dealing with other people and races, and in helping crew members with personal, psychological, and emotional problems.">
                   Ship's Counselor</p></div><div class="mani-field character"><p><a href="/manifest/tlaruk-akaigan">T'Laruk Akaigan</a></p></div>
