42ef6f111c3de471104e18bb43369b54b0b2544d
Ansel Colvin <anselcolvin@gmail.com>
2018-09-12 20:12:20 -0400

    Update

--- template/crew.php
+++ template/crew.php
@@ -90,6 +90,7 @@ These could be SPANs or DIVs and with entirely different classes.
    This would also need CSS to make it actually full width.
    */
    ?>
+   <br/><br/>
 
 
    <p><strong><span class="key">History:</span></strong></p>
