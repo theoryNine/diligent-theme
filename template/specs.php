0a916dd748ff6f194971bbf111f74fc34e8ae22b
Ansel Colvin <anselcolvin@gmail.com>
2018-09-21 01:01:40 -0400

    Update specs

--- template/specs.php
+++ template/specs.php
@@ -23,17 +23,15 @@ get_header(); ?>
          <div class="subnav"><h3><a href="#class_background">background</a></h3></div>
       </div>
       <div class="section layout" id="layout"><h1 class="text-center"><span class="heavy">DECK</span> layout</h1><br>
-         <ul>
-            <li>Deck 1: <em>Fore:</em> <a href="/wp-content/themes/diligent/images/diligent-bridge-blueprint.jpg" target="_blank">Main Bridge, Captain's Ready Room, Observation Lounge</a> - <em>Aft:</em> Cargobay 1, Aft Dorsal Torpedo Launcher</li>
-            <li>Deck 2: <a href="/wp-content/uploads/2018/09/captains_quarters.jpg" target="_blank">Captain's Quarters</a>, <a href="/wp-content/uploads/2018/09/officer_quarters.jpg" target="_blank">Senior Officer's Quarters</a>, <a href="/wp-content/uploads/2018/09/mess_hall.jpg" target="_blank">Ship's Mess and Galley</a>, Officers Lounge, Deuterium Fuel Tanks, Gymnasium</li>
-            <li>Deck 3: Transporter Rooms 1, <a href="/wp-content/uploads/2018/09/vipquarters.jpg" target="_blank">VIP Quarters</a>, <a href="/wp-content/uploads/2018/09/crewquarters.jpg" target="_blank">Crew Quarters</a>, Science Labs, Aeroponics, Astrometrics, Holosuites 1 - 4, Conference Rooms</li>
-            <li>Deck 4: Computer Core Control Room, <a href="/wp-content/uploads/2018/09/engineering.jpg" target="_blank">Main Engineering</a>, Engineering Support, Shuttle Bay</li>
-            <li>Deck 5: Transporter Rooms 2 and 3, <a href="/wp-content/uploads/2018/09/sickbay.jpg" target="_blank">Medical Bay</a>, <a href="/wp-content/uploads/2018/09/aft_lounge.jpg" target="_blank">Aft Lounge</a>, Navigational Deflector Control, Security Center</li>
-            <li>Deck 6: Fusion Reactors, Anti-Matter Storage Pods, Cargobay 2 and 3, <a href="/wp-content/uploads/2018/09/crewquarters.jpg" target="_blank">Crew Quarters</a></li>
-            <li>Deck 7: Forward Torpedo Launchers</li>
-            <li>Deck 8: Emergency Power Generators, Rear Torpedo Launchers</li>
-            <li>Deck 9: <em>non-habitable</em> Engineering Crawlspace, Mechanical</li>
-         </ul>
+            <p>Deck 1: <em>Fore:</em> <a href="/wp-content/themes/diligent/images/diligent-bridge-blueprint.jpg" target="_blank">Main Bridge, Captain's Ready Room, Observation Lounge</a> - <em>Aft:</em> Cargobay 1, Aft Dorsal Torpedo Launcher<br />
+            Deck 2: <a href="/wp-content/uploads/2018/09/captains_quarters.jpg" target="_blank">Captain's Quarters</a>, <a href="/wp-content/uploads/2018/09/officer_quarters.jpg" target="_blank">Senior Officer's Quarters</a>, <a href="/wp-content/uploads/2018/09/mess_hall.jpg" target="_blank">Ship's Mess and Galley</a>, Officers Lounge, Deuterium Fuel Tanks, Gymnasium<br />
+            Deck 3: Transporter Rooms 1, <a href="/wp-content/uploads/2018/09/vipquarters.jpg" target="_blank">VIP Quarters</a>, <a href="/wp-content/uploads/2018/09/crewquarters.jpg" target="_blank">Crew Quarters</a>, Science Labs, Aeroponics, Astrometrics, Holosuites 1 - 4, Conference Rooms<br />
+            Deck 4: Computer Core Control Room, <a href="/wp-content/uploads/2018/09/engineering.jpg" target="_blank">Main Engineering</a>, Engineering Support, Shuttle Bay<br />
+            Deck 5: Transporter Rooms 2 and 3, <a href="/wp-content/uploads/2018/09/sickbay.jpg" target="_blank">Medical Bay</a>, <a href="/wp-content/uploads/2018/09/aft_lounge.jpg" target="_blank">Aft Lounge</a>, Navigational Deflector Control, Security Center<br />
+            Deck 6: Fusion Reactors, Anti-Matter Storage Pods, Cargobay 2 and 3, <a href="/wp-content/uploads/2018/09/crewquarters.jpg" target="_blank">Crew Quarters</a><br />
+            Deck 7: Forward Torpedo Launchers<br />
+            Deck 8: Emergency Power Generators, Rear Torpedo Launchers<br />
+            Deck 9: <em>non-habitable</em> Engineering Crawlspace, Mechanical</p>
          <p class="disclaimer"><em>Main Engineering by: <a href="https://www.deviantart.com/falke2009" target="_blank">Falke</a><br />
          All other interior illustrations were expertly created by: <a href="https://www.deviantart.com/bobye2" target="_blank">bobeye</a></em>
          </p>
