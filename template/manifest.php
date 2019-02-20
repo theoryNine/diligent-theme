95508b3e2d6700c885f0106e3f9fa1f2c24f9869
Ansel Colvin <anselcolvin@gmail.com>
2019-02-19 19:30:50 -0500

    Manifest Update

--- template/manifest.php
+++ template/manifest.php
@@ -88,13 +88,13 @@ These could be SPANs or DIVs and with entirely different classes.
 <div class="intro section">
       <h1 class="text-center">crew <span class="heavy">MANIFEST</span></h1>
       <p class="text-center">The writers and dreamers of the <em>Diligent</em>.</p>
-      <p class="text-center department-links">Command - Engineering - Flight Control - Intelligence - Medical - Operations - Science - Tactical/Security</p>
+      <p class="text-center department-links"><a href="#command">Command</a> - Engineering - Flight Control - Intelligence - Medical - Operations - Science - Tactical/Security</p>
 </div>
 
 <div class="section">
    <div class="manifest">
       <div class="department section">
-         <h3><a href="https://wiki.bravofleet.com/index.php?title=Command_Department" target="_blank">COMMAND</a></h3>
+         <h3><a href="https://wiki.bravofleet.com/index.php?title=Command_Department" target="_blank" id="command">COMMAND</a></h3>
          <div class="row">
             <div class="mani-field rank"><img src="/wp-content/themes/diligent/images/pips/navy_gold/r-o5.png" /></div><div class="mani-field position"><p class="tooltips" title="
             Every Starfleet Facility has a single commanding officer responsible for the safety of their crew and the success of their missions. They have 
