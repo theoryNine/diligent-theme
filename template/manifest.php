55a40207628e7aeae736b637e9ee4d079e6b4c62
Ansel Colvin <anselcolvin@gmail.com>
2019-03-03 13:27:35 -0500

    Cal to CSO

--- template/manifest.php
+++ template/manifest.php
@@ -206,7 +206,7 @@ These could be SPANs or DIVs and with entirely different classes.
       <div class="department section open-npc">      
          <h3><a href="https://wiki.bravofleet.com/index.php?title=Science_Department" target="_blank">SCIENCE</a></h3>
          <div class="row open">
-            <div class="mani-field rank"><img src="/wp-content/themes/diligent/images/pips/blank_officer/black.png" /></div><div class="mani-field position"><p>Chief Science Officer</p></div><div class="mani-field character"><p><a href="/enlist">Position Available</a></p></div>
+            <div class="mani-field rank"><img src="/wp-content/themes/diligent/images/pips/navy_gold/t-o4.png" /></div><div class="mani-field position"><p>Chief Science Officer</p></div><div class="mani-field character"><p><a href="/manifest/caelestis-everett">Cælestis Everett</a></p></div>
          </div>
          <div class="row open">
             <div class="mani-field rank"><img src="/wp-content/themes/diligent/images/pips/blank_officer/black.png" /></div><div class="mani-field position"><p>Assistant Chief Science Officer</p></div><div class="mani-field character"><p><a href="/enlist">Position Available</a></p></div>
