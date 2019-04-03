ab6a431dad9f486852796e087ab49c48a9f9923b
Ansel Colvin <anselcolvin@gmail.com>
2019-04-03 18:07:22 -0400

    Fixing buttons

--- template/functions.php
+++ template/functions.php
@@ -29,4 +29,12 @@ function custom_titles( $title, $sep ) {
     return $title;
 }
 
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
+
  ?>
